e0100d836fdff318a871df6c08ac7d8263072c38
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-05-31 14:48:54 +0530

    basic api

--- expressjs/expserver.js
+++ expressjs/expserver.js
@@ -5,10 +5,13 @@ const port=4000;
 const bodyParser=require('body-parser'); //its used to get access to the req.body 
 // GET ==Read
 app.use(bodyParser.urlencoded({extended:false}))
-app.get('/',(req,res)=>{
+app.get('/amit',(req,res)=>{
       // res.send('Hello World')
       // sending html pages
       res.sendFile(path.join(__dirname+'/index.html'))
+      res.json({
+            name:'amit'
+      })
 })
 
 // Post == create
@@ -16,6 +19,13 @@ app.post('/jimmy',(req,res)=>{
       // res.send('Done');
       console.log(req.body)
       res.send(`<h1> Your name is : ${req.body.name}</h1> <h1> Your email  is : ${req.body.email}</h1>`)
+})
+
+app.post('/amit/register',(req,res)=>{
+      res.json({
+            name:'amit',
+            email:'hell0'
+      })
 })
 
 
