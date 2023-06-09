4874bd3c48f4e7c30df52efc20def5cf4f9ad8d8
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-06-10 00:24:46 +0530

    jwt new

--- auth/controllers/form.js
+++ auth/controllers/form.js
@@ -1,9 +1,15 @@
 const path = require('path');
-const form = (req, res) => {
+const Users=require('../db/schema')
+const jwt=require('jsonwebtoken');
+const { decode } = require('punycode');
+const form =async (req, res) => {
   let indexPath;
   const token=req.cookies.token
     if(token){
+    const decoded=jwt.verify(token,process.env.SECRET_KEY)
      indexPath = path.resolve(__dirname, '../','logout.html')
+     req.user= await Users.findById(decoded._id)
+     console.log(req.user)
   }
   else{
      indexPath = path.resolve(__dirname, '../','index.html')
