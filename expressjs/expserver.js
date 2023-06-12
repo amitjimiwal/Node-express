0840f28015edd9efb199f90cc48046d7fce013ba
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-06-12 15:43:52 +0530

    handling wrong api request

--- expressjs/expserver.js
+++ expressjs/expserver.js
@@ -1,13 +1,19 @@
-const express=require('express')
-const path=require('path')
-const app=express();
-const port=4000;
-const bodyParser=require('body-parser'); //its used to get access to the req.body 
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 4000;
+const bodyParser = require("body-parser"); //its used to get access to the req.body
 // GET ==Read
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 const router = require("./routes/userRoute");
-app.use(router)
-app.use(express.json())
-app.listen(port,()=>{
-      console.log(`server started on port ${port}`)
-})
+app.use(router);
+app.use(express.json());
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
+});
